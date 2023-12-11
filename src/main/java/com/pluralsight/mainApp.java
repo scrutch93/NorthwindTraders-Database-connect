16c1381ca91f4bf3cefc4a16aeb9c8ce62bc3780
Steven <stevccru@icloud.com>
2023-12-11 15:30:21 -0500

    initial commit

---
+++ src/main/java/com/pluralsight/mainApp.java
@@ -0,0 +1,39 @@
+package com.pluralsight;
+
+import java.sql.*;
+
+public class mainApp {
+
+        public static void main(String[] args) {
+
+
+            try {
+                Connection connection = DriverManager.getConnection(url, user, password);
+                // Perform database operations here
+
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products ");
+
+
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                while (resultSet.next()) {
+                    // Get the 1st and 2nd fields returned from the query
+                    // based on the SELECT statement
+                    System.out.printf("Product Id: %d\nName: %s\nPrice: %.2f\nStock: %d\n------------------\n",
+                            resultSet.getInt(1),
+                            resultSet.getString(2),
+                            resultSet.getFloat(3),
+                            resultSet.getInt(4));
+                }
+
+
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
