5f4a322cc42ad8346e37a0c809daef4400d56dcf
Steven <stevccru@icloud.com>
2023-12-12 13:47:10 -0500

    added try with resources

--- src/main/java/com/pluralsight/mainApp.java
+++ src/main/java/com/pluralsight/mainApp.java
@@ -5,21 +5,25 @@ import java.util.Scanner;
 
 public class mainApp {
     static String url = "jdbc:mysql://localhost:3306/northwind";
-    static  String user = "root";
-    static String password = "Supersonic93!";
+    static  String user = "";
+    static String password = "";
+    static ResultSet resultSet = null;
+    static PreparedStatement preparedStatement = null;
+    static Connection connection = null;
+    static Scanner input = new Scanner(System.in);
+
 
         public static void main(String[] args) throws SQLException {
 
 
-            Scanner input = new Scanner(System.in);
-
             System.out.println("What would you like to do?");
             System.out.println(" 1) Display all products");
             System.out.println(" 2) Display all customers");
-            System.out.println(" 3) Exit");
+            System.out.println(" 3) Display all categories");
+            System.out.println(" 4) Exit");
             System.out.println("Select an option: ");
-
             int selection = input.nextInt();
+
 
             switch (selection) {
                 case 1:
@@ -29,6 +33,10 @@ public class mainApp {
                     getCustomers();
                     break;
                 case 3:
+                    getAllCategories();
+                    getProductsFromCategory();
+                    break;
+                case 4:
                     System.out.println("Later!");
                     System.exit(0);
             }
@@ -38,18 +46,17 @@ public class mainApp {
         }
     public static void getProducts() throws SQLException {
 
-        ResultSet resultSet = null;
-        PreparedStatement preparedStatement = null;
-        Connection connection = null;
         try {
+            //This connects to MySQL with login info
             connection = DriverManager.getConnection(url, user, password);
-            // Perform database operations here
 
+            //This sets up the query prompt for SQL
             preparedStatement = connection.prepareStatement(
                     "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products ");
 
-
+            //This takes the results from the executed query.
             resultSet = preparedStatement.executeQuery();
+
 
             while (resultSet.next()) {
                 // Get the 1st and 2nd fields returned from the query
@@ -75,15 +82,13 @@ public class mainApp {
 
     public static void getCustomers() throws SQLException {
 
-        ResultSet resultSet = null;
-        PreparedStatement preparedStatement = null;
-        Connection connection = null;
         try {
             connection = DriverManager.getConnection(url, user, password);
             // Perform database operations here
 
             preparedStatement = connection.prepareStatement(
-                    "SELECT ContactName, CompanyName, City, Country, Phone FROM customers ");
+                    "SELECT ContactName, CompanyName, City, Country, Phone FROM customers " +
+                            "ORDER BY Country");
 
 
             resultSet = preparedStatement.executeQuery();
@@ -111,11 +116,55 @@ public class mainApp {
         }
     }
 
-
-
-
-
+    //Below shows a try with resources approach
+    public static void getAllCategories() throws SQLException {
+        try (
+                Connection connection = DriverManager.getConnection(url, user, password);
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "SELECT CategoryID, CategoryName FROM categories ORDER BY CategoryID");
+                ResultSet resultSet = preparedStatement.executeQuery()
+        ) {
+            while (resultSet.next()) {
+                System.out.printf("Category ID: %d  Company Name: %s\n",
+                        resultSet.getInt(1),
+                        resultSet.getString(2));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
+
+    public static void getProductsFromCategory() {
+        System.out.println("Enter a category ID to display all respective products:");
+        int selection = input.nextInt();
+
+        //This try with resources approach first uses try to insert objects that could fail
+        try (
+                Connection connection = DriverManager.getConnection(url, user, password);
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "SELECT * FROM products WHERE CategoryID = ?")
+        ) {
+            preparedStatement.setInt(1, selection);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                System.out.println("Showing all results from CategoryID " + selection + ":");
+                System.out.println("--------------------");
+                while (resultSet.next()) {
+                    System.out.printf("Product Id: %d\nName: %s\nPrice: %.2f\nStock: %d\n------------------\n",
+                            resultSet.getInt(1),
+                            resultSet.getString(2),
+                            resultSet.getFloat(3),
+                            resultSet.getInt(4));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
 
 
 
